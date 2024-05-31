68ad5bd3725c2fd9f59577a5968f65f7affa56e4
nhathao19 <141889402+nhathao19@users.noreply.github.com>
2024-05-31 12:42:24 +0700

    Add import

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import axios from "axios";
 import { useNavigate } from "react-router-dom"
+import './styles/style.css';
+
 
 const Register = () => {
     const [name, setName] = useState('');
